da7081c766e3e2005a0c2e4fce8fb4e287f4c11f
heshan <heshan.sandaruwan55875@gmail.com>
2019-05-11 20:55:23 +0530

    DashBoard

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,10 +3,12 @@ import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { clearCurrentProfile } from '../../actions/profileActions';
 
 class Navbar extends Component {
   onLogoutClick(e){
     e.preventDefault();
+    this.props.clearCurrentProfile();
     this.props.logoutUser();
   }
 
@@ -60,6 +62,7 @@ class Navbar extends Component {
 
 Navbar.proptype = {
   logoutUser: PropTypes.func.isRequired,
+  clearCurrentProfile: PropTypes.func,
   auth: PropTypes.object.isRequired
 }
 
@@ -67,4 +70,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { logoutUser })(Navbar);
+export default connect(mapStateToProps, { logoutUser, clearCurrentProfile })(Navbar);
